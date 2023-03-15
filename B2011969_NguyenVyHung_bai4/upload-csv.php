14a5c5281f1487f302eca883f17ec92441c3d90a
Anh3Hung <122862364+Anh3Hung@users.noreply.github.com>
2023-03-15 20:39:10 +0700

    Delete B2011969_NguyenVyHung_bai4 directory

--- B2011969_NguyenVyHung_bai4/upload-csv.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-    <form action="upload-csv-processing.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="file"/>
-        <input type="submit" name="submit" value="Import"/>
-    </form>
-</body>
-</html>
